57408c419e96fd58062274982362047f2a08d6c6
Polsani sandeep <polsani.sandeep@gmail.com>
2020-09-13 23:50:57 +0530

    aws resource

--- main.tf
+++ main.tf
@@ -10,3 +10,10 @@ provider "aws" {
  profile = "default"
   region  = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+  ami = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  subnet_id="subnet-02e6f3ed718c3c8ab"
+  key_name="main"
+}
